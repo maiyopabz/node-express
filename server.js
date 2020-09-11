8f3a7a150a3e4edb54dbbbbb6707791d9afb4ada
Maiyo P <maiyopabz@gmail.com>
2020-09-11 11:55:24 -0700

    Express Router

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/campsites', campsiteRouter);
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
 
 app.use((req, res) => {
     res.statusCode = 200;
